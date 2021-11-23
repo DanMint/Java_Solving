8530b170dc3d7b01dbd3cf88d4f8916108495c82
Daniel Mints <67702241+DanMint@users.noreply.github.com>
2021-11-23 09:48:24 +0100

    Create thread2.java

---
+++ Working_With_Threads/Even_Odd/thread2.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class thread2 implements Runnable{
+    int processID;
+    ArrayList<Integer> values = new ArrayList<>();
+
+    thread2 (int num, ArrayList<Integer> a) {
+        processID = num;
+        values = a;
+    }
+
+    @Override
+    public void run() {
+        for (Integer value : values) {
+            if (value % 2 == 0)
+                System.out.println(value + " The thread used is: " + processID);
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
