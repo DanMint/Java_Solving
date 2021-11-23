8e2421dfb9ab3cd64acff5a2ae825a90e51543fe
Daniel Mints <67702241+DanMint@users.noreply.github.com>
2021-11-23 14:08:46 +0100

    Create Main.java

---
+++ Working_With_Thread/Creating_Many_Threads/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        /// here I want to store the threads and use them when the prev threads ends
+        ArrayList<Thread> allThreads = new ArrayList<>(10);
+
+        thread t = new thread(0, 10);
+        Thread t0 = new Thread(t);
+        allThreads.add(t0);
+
+        int toPass = t.get();
+        System.out.println(toPass);
+
+        for (int i = 1; i < 11; i ++) {
+            thread t1 = new thread(i, -1);
+            Thread tTemp = new Thread(t1);
+            allThreads.add(tTemp);
+        }
+
+        for (int i = 0; i < allThreads.size(); i ++) {
+            allThreads.get(i).start();
+        }
+
+    }
+}
